3beb1cf19f09eb2aa2b49d69dc4886d21d80dcc4
Shruti Pinjarkar <132293976+shruti200216@users.noreply.github.com>
2023-06-29 01:54:10 +0530

    main

---
+++ Face_Recognition_Attendance_System-main/README.md
@@ -0,0 +1,123 @@
+# Face_Recognition_Attendance_System
+
+The objective of this project is to process live video-stream of students entering their classroom and generate the list of students attending the class. The system is coded in Python using OpenCv , Tkinter and MySQL.
+This project has been developed as a part of the Microsoft Engage 2022 program.
+
+
+### Installation
+
+## Python
+
+- Download Python from https://www.python.org/downloads/
+
+## MySQL
+
+- Download MySQL from https://dev.mysql.com/downloads/installer/
+
+
+## Required steps
+
+- Clone the repo:
+```$ git clone https://github.com/guptaishika128/Face_Recognition_Attendance_System.git```
+
+**Then you need to connect to database.** 
+- To do that install MySQL with **username = root** and **password = 12345678** and port 3306 (By default)
+
+- Now open MySQL Workbench and click on default connection
+![Image](/Screenshots/MySQL.png)
+
+- Enter password stated above in the window
+![Image](/Screenshots/mysql1.png)
+
+- Now create two schemas one with name **face_recognizer** 
+![Image](/Screenshots/schema1.png)
+
+- Other schema with name **mydata**
+![Image](/Screenshots/schema2.png)
+
+- Now as shown below click on Data Import
+![Image](/Screenshots/DataImport.png)
+
+- Clicking on Import from Dump Project Folder find the location where you cloned this project under that click on **Database** folder 
+![Image](/Screenshots/findingdatabasefolder.png)
+
+- You will see two files select both and click on Start Import
+![Image](/Screenshots/import.png)
+
+- **You have connected**. Refreshing you will be able to see student and register sql files under schemas
+![Image](/Screenshots/Connected.png)
+
+- Now run **login.py** 
+
+
+## ⚙️ HOW THE SYSTEM WORKS?
+
+This system works accordingly with a series of step explained below:
+
+1. **LOGIN PAGE**:
+
+First the admin will login using either default credentials **username=user@123** and **password=user@123** or can create a new account.
+
+![Image](/Screenshots/Loginpage.png)
+
+2. After entering right credentials welcome message appears.
+
+![Image](/Screenshots/Welcomemessageappears.png)
+
+3. Now the main page of face recognition attendance system appears
+
+![Image](/Screenshots/Facerecognitionmainpage.png)
+
+4. First Clicking on **Student Details** opens the page to register students and see their details. Students must have unique ID 
+
+![Image](/Screenshots/Registeringstudents.png)
+
+5. After saving details click on Take Image option to capture student's face . 100 samples will be taken.
+
+![Image](/Screenshots/Capturingimage.png)
+
+6. Next on the main page click on **Train Image** after that cick on Train Data to train samples for detection and recognition.
+
+![Image](/Screenshots/Trainingdata.png)
+
+7. Now click on **Take Attendance** to recognize face . If the computer recognizes the face details are shown otherwise 'Unknown Image' message is shown
+
+![Image](/Screenshots/TakingAttendance.png)
+
+8. Simultaneously, a csv file **attendance.csv** will be updated with the ID,Roll No, Name of the student ,Department, Date and Time at which his/her face was recognized.
+
+![Image](/Screenshots/Attendancesaved.png)
+
+9. On the main page click on **Attendance Report** and then import attendance.csv to view attendance report of students. Attendance can also be exported .
+
+![Image](/Screenshots/Importingattendance.png)
+
+![Image](/Screenshots/Attendanceimported.png)
+
+
+
+
+## Built With
+
+* [OpenCV](http://docs.opencv.org/3.1.0/) - Implementation of Algorithms
+* [Tkinter](https://docs.python.org/2/library/tkinter.html) - GUI Implementation
+* [MySQL](https://docs.oracle.com/en-us/iaas/mysql-database/doc/getting-started.html) - Database
+* [HAAR-CASCADE CLASSIFIER](https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html)
+* [LocalBinaryPatternHistogram (LBPH) recognizer](https://docs.opencv.org/master/df/d25/classcv_1_1face_1_1LBPHFaceRecognizer.html)
+* [PIL](https://pillow.readthedocs.io/en/stable/)
+
+
+**Following functionalities can be performed: <br>**
+• Login <br>
+• New User Account can also be created. <br>
+• If forgotten the login credentials password can be changed <br>
+• Register new students to the system. Students can be filtered by Name or Department<br>
+• Take photo sample of them <br>
+• Train the model <br>
+• Take attendance by scanning face <br>
+• View attendance report of all students by importing csv. Also attendance can be exported too. <br>
+
+
+## ❤️ Owner
+Made with ❤️  by Ishika Gupta
+
---
+++ Face_Recognition_Attendance_System-main/face_recog.py
@@ -0,0 +1,137 @@
+import cv2
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+from tkinter import messagebox
+from time import strftime
+from datetime import datetime
+import mysql.connector
+import os
+import numpy as np
+
+
+
+class Face_Recognition:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1536x816+0+0")
+        self.root.title("Face Recognition")
+        self.root.configure(bg="light blue")
+
+        title_lbl=Label(root,text="FACE RECOGNITION ",font=("times new roman",40,"bold"),bg="orchid",fg="white")
+        title_lbl.place(x=0,y=0,width=1536,height=50)
+
+        def time():
+            string = strftime('%H:%M:%S %p')
+            lbl.config(text = string)
+            lbl.after (1000, time)
+
+        lbl = Label(title_lbl, font=('times new roman',14, 'bold'), background='black', foreground ='orchid')
+        lbl.place(x=5, y=10,width=120,height=40)
+        time()
+
+        
+        img_11=Image.open("Images//FACERECOG1.jpg")
+        img_11=img_11.resize((1536,816),Image.ANTIALIAS)
+        self.img1=ImageTk.PhotoImage(img_11)
+
+        lb1=Label(self.root,image=self.img1)
+        lb1.place(x=5,y=50,width=1530,height=743)
+
+        b1_1=Button(root,text="Take Attendance",command=self.face_recogn,cursor="hand2",font=("times new roman",30,"bold"),bg="blue",fg="white")
+        b1_1.place(x=450,y=670,width=650,height=50)
+
+    #****************Attendance*****************************
+
+    def mark_attendance(self,i,r,n,d):
+        with open("attendance.csv","r+",newline="\n") as f:
+            myDataList=f.readlines()
+            name_list=[]
+            for line in myDataList:
+                entry=line.split((","))
+                name_list.append(entry[0])
+            if((i not in name_list) and (r not in name_list) and (n not in name_list) and (d not in name_list)):
+                now=datetime.now()
+                d1=now.strftime("%d/%m/%Y")
+                dtString=now.strftime("%H:%M:%S")
+                f.writelines(f"\n{i},{r},{n},{d},{dtString},{d1},Present")
+
+
+    #*****************Face Recognition******************************
+    def face_recogn(self):
+        def draw_boundary(img,classifier,scaleFactor,minNeighbors,color,text,clf):
+            gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            features=classifier.detectMultiScale(gray_image,scaleFactor,minNeighbors)
+
+            coordi=[]
+
+            for(x,y,w,h) in features:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+                id,predict=clf.predict(gray_image[y:y+h,x:x+w])
+                confidence=int((100*(1-predict/300)))
+
+                conn=mysql.connector.connect(host="localhost",username="shruti",password="12345678",database="face_recognizer")
+                my_cursor=conn.cursor()                
+
+                my_cursor.execute("select Name from student where Student_id="+str(id))
+                n=my_cursor.fetchone()
+                # n="+".join(n)
+                n="shruti"
+
+                my_cursor.execute("select Roll from student where Student_id="+str(id))
+                r=my_cursor.fetchone()
+                # r="+".join(r)
+                r=18
+
+                my_cursor.execute("select Dep from student where Student_id="+str(id))
+                d=my_cursor.fetchone()
+                # d="+".join(d)
+                d="IT"
+
+                my_cursor.execute("select Student_id from student where Student_id="+str(id))
+                i=my_cursor.fetchone()
+                # i="+".join(i)
+                i=20104016
+
+
+
+                if confidence>75:
+                    cv2.putText(img,f"ID:{i}",(x,y-80),cv2.FONT_HERSHEY_COMPLEX,0.8,(128, 0, 0),3)
+                    cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(128, 0, 0),3)
+                    cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(128, 0, 0),3)
+                    cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(128, 0, 0),3)
+                    self.mark_attendance(i,r,n,d)
+                else:
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+                    cv2.putText(img,"Unknown Face",(x,y-7),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3) 
+
+                coordi=[x,y,w,h]
+
+            return coordi
+
+        def recognize(img,clf,faceCascade):
+            coordi=draw_boundary(img,faceCascade,1.1,10,(255,255,255),"Face",clf)
+            return img
+
+        faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.read("Trainner.xml")
+
+        videocam=cv2.VideoCapture(0)
+
+        while True:
+            ret,img=videocam.read()
+            img=recognize(img,clf,faceCascade)
+            cv2.imshow("Welcome To Face Recognition",img)
+
+            if cv2.waitKey(1)==13:
+                break
+        videocam.release()
+        cv2.destroyAllWindows()                      
+
+                    
+
+if __name__ == "__main__":
+    root = Tk()
+    obj=Face_Recognition(root)
+    root.mainloop()              
